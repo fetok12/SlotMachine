20e13ce8330b1c36b62edb482c361d61491792dc
fetok12 <30753020+fetok12@users.noreply.github.com>
2021-08-11 03:54:43 +0300

    Build Spin Probability Logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -97,8 +97,144 @@ public class GameManager : MonoBehaviour
 
     private bool ShowBingoAnim = false;
     private float BingoMagnitude;
-   
+    public void CreateRounds()
+    {
+        Rounds.Clear();
+        // Randomize Order
+        //SpinResults.Shuffle();
+
+        List<int> CreatedRounds = new List<int>();
+
+        for (int i = 0; i < SpinResults.Count; i++)
+        {
+            int LeftValue = 0;
+
+            // Pick random round number for every block 
+            for (int j = 0; j < SpinResults[i].SpinProbability; j++)
+            {
+                int RightValue = LeftValue + SpinResults[i].BlockIntervals[j] - 1;
+                
+                int RandomRound = GenerateRoundNumber(ref CreatedRounds, LeftValue, RightValue);
+                
+                // if RandomRound equals to -1, Rerun the CreateRounds function.
+                if(RandomRound == -1)
+                {
+                    foreach (var item in SpinResults)
+                    {
+                        item.BlockIntervals.Shuffle();
+                    }
+                    CreateRounds();
+                    return;
+                }
+                Rounds.Add(RandomRound, SpinResults[i]);
+                LeftValue = RightValue + 1;
+            }
+
+        }
+        PlayerPersistence.SaveData(Rounds, currentRound);
     }
+
+
+    public int GenerateRoundNumber(ref List<int> CreatedRounds, int LeftValue, int RightValue)
+    {
+        List<int> AllNumbers = new List<int>();
+        for (int i = LeftValue; i < RightValue + 1; i++)
+        {
+            AllNumbers.Add(i);
+        }
+
+        List<int> CandidateNumbers = AllNumbers.Except(CreatedRounds).ToList();
+
+        if(CandidateNumbers.Count > 0)
+        {
+            int index = UnityEngine.Random.Range(0, CandidateNumbers.Count);
+            CreatedRounds.Add(CandidateNumbers[index]);
+            return CandidateNumbers[index];
+        }
+
+        return -1;
+    }
+
+
+    private void Awake()
+    {
+        Row.SlotStatusEvent += UpdateSlotStatus;
+    }
+    private void Start()
+    {
+        if (PlayerPersistence.LoadData(ref Rounds, ref currentRound, SpinResults) == -1)
+            CreateRounds();
+        else
+        {
+            LoadPreviousState();
+
+        }
+    
+        ShowLogs();
+
+    }
+
+
+    public void PlayRound()
+    {
+        if (currentRound == Rounds.Count)
+            ResetGameState();
+
+        // Check if slots are ready
+        if (Slots.Values.Any(x => x != SlotStatus.Inactive)) return;
+      
+
+        // Check if all coins in the list are the same.
+        bool isBingo = !Rounds[currentRound].CoinPositions.Distinct().Skip(1).Any();
+
+        ShowBingoAnim = isBingo;
+        BingoMagnitude = Rounds[currentRound].BingoDensity;
+
+        // Send Rows SpinResult
+        SpinEvent?.Invoke(Rounds[currentRound]);
+
+        RoundIndicatorUI.GetComponent<TextMeshProUGUI>().text = $"Round: {currentRound + 1}";
+
+        currentRound++;
+      
+        PlayerPersistence.SaveData(Rounds, currentRound);
+
+        Debug.Log(currentRound);
+    }
+
+    public void UpdateSlotStatus(int SlotNumber, SlotStatus Status)
+    {
+        if (!Slots.ContainsKey(SlotNumber))
+        {
+            Slots.Add(SlotNumber, Status);
+            return;
+        }
+        Slots[SlotNumber] = Status;
+
+        // Check if all slots stopped
+        bool isStop = !Slots.Values.Any(x => x != SlotStatus.Inactive);
+      
+        if (isStop)
+        {
+            // If it's bingo
+            if (ShowBingoAnim)
+            {
+                BingoParticle.SetActive(true);
+                var ps = BingoParticle.GetComponent<ParticleSystem>().main;
+                ps.simulationSpeed = 2.33f;
+
+                //Apply BingoMagnitude
+                var emission = BingoParticle.GetComponent<ParticleSystem>().emission;
+                Debug.Log(BingoMagnitude);
+                emission.rateOverTime = BingoMagnitude;
+
+                BingoParticle.GetComponent<ParticleSystem>().Play();
+            }
+        }
+
+    }
+
+
 }
 
 
