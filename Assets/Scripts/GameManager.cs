1f1e86fe8d9e117c1a7414dbf50d5d9feb096ac5
fetok12 <30753020+fetok12@users.noreply.github.com>
2021-08-11 03:46:57 +0300

    Create Coin Types

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using Random = System.Random;
+
+public struct CoinType
+{
+    public const float Jackpot = -1.25f;
+    public const float A = 1.25f;
+    public const float Bonus = 3.75f;
+    public const float Seven = 6.25f;
+    public const float Wild = 8.75f;
+}
+
+
+
+public class GameManager : MonoBehaviour
+{
+  
+}
